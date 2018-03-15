b9db215ec9b349e1beb78545bc14071d1b2552ea
0xADADA <0xadada.pub@0xadada.pub>
2018-03-15 09:42:06 -0400

    WIP

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,30 @@
 import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
-import { set } from '@ember/object';
+import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+import { inject as service } from '@ember/service';
+import EmberObject, { get, set } from '@ember/object';
 
-export default Route.extend(ApplicationRouteMixin, {
+export default Route.extend(ApplicationRouteMixin, UnauthenticatedRouteMixin, {
+  auth: service(),
+
+  async model() {
+    let isAuthenticated = get(this, 'session.isAuthenticated');
+
+    // the index route is reserved for authenticated users as the "Dashboard"
+    // of the app. Rather than use AuthenticatedRouteMixin which redirects users
+    // to /login, we want to redirect to /welcome
+    if (isAuthenticated) {
+      // everyone else is sent to to `welcome` route
+      return await get(this, 'auth').getUser();
+    } else {
+      // return an empty user
+      return EmberObject.create({
+        user: EmberObject.create({}),
+        errors: []
+      });
+    }
+  },
+
   actions: {
     async loading(transition) {
       // Toggle progress bar when transition is resolving the model promise
